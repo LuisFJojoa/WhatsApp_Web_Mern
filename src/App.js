f6261ac42ecf97dd3ebf3605fa127ad8315919bc
ljojoa <luis.jojoa.q@gmail.com>
2021-04-11 12:55:14 -0500

    Add Styles in components to separate them

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Sidebar from './components/Sidebar/Sidebar';
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a MERN WhatsApp clone</h1>
-
-      
-      <Sidebar/>
-      <Chat/>
+      <div class="app__body">
+        <Sidebar/>
+        <Chat/>
+      </div>
     </div>
   );
 }
