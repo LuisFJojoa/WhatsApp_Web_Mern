8ac17d6d4b8a3d72382f144fd0eb08913038d666
ljojoa <luis.jojoa.q@gmail.com>
2021-04-10 20:28:47 -0500

    Create chat and sidebar components and create firebaseconfig file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Chat from './components/chat/Chat';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Let's build a MERN WhatsApp clone</h1>
+
+      
+      <Sidebar/>
+      <Chat/>
     </div>
   );
 }
