d62bdd505cfad1df714e2c5c6f8df2e5fbc6d78c
ljojoa <luis.jojoa.q@gmail.com>
2021-04-20 23:15:10 -0500

    CSS Migration to styled components finished

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Chat_Content from '../Chat_Content/Chat_Content'
 import Chat_Header from '../Chat_Header/Chat_Header'
 import Chat_InputText from '../Chat_InputText/Chat_InputText'
-import './Chat.css'
+import {Chat_} from './ChatStyled'
 
 function Chat() {
     return (
-        <div className="chat">
+        <Chat_>
            <Chat_Header/> 
            <Chat_Content/>
            <Chat_InputText/>
-        </div>
+        </Chat_>
     )
 }
 
